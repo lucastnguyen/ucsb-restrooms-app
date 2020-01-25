90eefaab8fc00c24c40e7656ddfca16cd16b1e06
Lucas Nguyen <lucas@computer.local>
2020-01-24 17:11:20 -0800

    working on implementing collapsible list of bathrooms to Bathrooms tab

--- app/UCSBB/screens/BathroomsScreen.js
+++ app/UCSBB/screens/BathroomsScreen.js
@@ -1,10 +1,62 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { CollapsibleBathroomsList } from '../components/CollapsibleBathroomsList';
+
+import { MonoText } from '../components/StyledText';
 
 export default function BathroomsScreen() {
   return (
-    <ScrollView style={styles.container}>
+    <View style={styles.container}>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}>
+      <View style={styles.getStartedContainer}>
+        <View
+          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+          <MonoText>Phelps 2501</MonoText>
+        </View>
+        <View
+          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+          <MonoText>Phelps 3501</MonoText>
+        </View>
+        <View
+          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+          <MonoText>South Hall 1636</MonoText>
+        </View>
+        <View
+          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+          <MonoText>North Hall 0000</MonoText>
+        </View>
+        <View
+          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+          <MonoText>Harold Frank Hall 0000</MonoText>
+        </View>
+        <View
+          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+          <MonoText>Broida Hall 0000</MonoText>
+        </View>
+        <View
+          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+          <MonoText>Girvetz Hall 0000</MonoText>
+        </View>
+        <View
+          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+          <MonoText>Life Sciences Building 0000</MonoText>
+        </View>
+      </View>
+
+    
+      <View
+          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+          <CollapsibleBathroomsList>CBL</CollapsibleBathroomsList>
+      </View>
     </ScrollView>
+    </View>
   );
 }
 
@@ -15,7 +67,19 @@ BathroomsScreen.navigationOptions = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    backgroundColor: "rgba(0, 0, 0, 0.1)"
+  },
+  contentContainer: {
+    paddingTop: 30,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  codeHighlightContainer: {
+    backgroundColor: 'rgba(0,0,0,0.05)',
+    borderRadius: 3,
+    paddingHorizontal: 4,
+  },
+  homeScreenFilename: {
+    marginVertical: 7,
   },
 });
