e432d8038d9c69d038be0299ad1331fbc7a26f6b
Lucas Nguyen <lucas@computer.local>
2020-01-24 17:45:23 -0800

    added collapsible bathrooms list with placeholder bathrooms, added color to list

--- app/UCSBB/screens/BathroomsScreen.js
+++ app/UCSBB/screens/BathroomsScreen.js
@@ -4,7 +4,9 @@ import {
   StyleSheet,
   Text,
   View,
+  Dimensions,
 } from 'react-native';
+import CollapsibleList from "react-native-collapsible-list";
 import { CollapsibleBathroomsList } from '../components/CollapsibleBathroomsList';
 
 import { MonoText } from '../components/StyledText';
@@ -15,46 +17,116 @@ export default function BathroomsScreen() {
     <ScrollView
       style={styles.container}
       contentContainerStyle={styles.contentContainer}>
-      <View style={styles.getStartedContainer}>
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-          <MonoText>Phelps 2501</MonoText>
+      <View style={styles.container}>
+          <CollapsibleList
+              numberOfVisibleItems={0}
+              wrapperStyle={styles.wrapperCollapsibleList}
+              buttonContent={
+                <View style={styles.button}>
+                  <Text style={styles.buttonText}>Phelps Hall</Text>
+                </View>
+              }>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Phelps 0000</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Phelps 0001</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Phelps 0002</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Phelps 0003</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Phelps 2501</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Phelps 3501</Text>
+              </View>
+          </CollapsibleList>
+          <CollapsibleList
+              numberOfVisibleItems={0}
+              wrapperStyle={styles.wrapperCollapsibleList}
+              buttonContent={
+                <View style={styles.button}>
+                  <Text style={styles.buttonText}>South Hall</Text>
+                </View>
+              }>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>South Hall 1636</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>South Hall 0001</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>South Hall 0002</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>South Hall 0003</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>South Hall 0004</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>South Hall 0005</Text>
+              </View>
+          </CollapsibleList>
+          <CollapsibleList
+              numberOfVisibleItems={0}
+              wrapperStyle={styles.wrapperCollapsibleList}
+              buttonContent={
+                <View style={styles.button}>
+                  <Text style={styles.buttonText}>Arts Building</Text>
+                </View>
+              }>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Arts Building 0000</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Arts Building 0001</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Arts Building 0002</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Arts Building 0003</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Arts Building 0004</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Arts Building 0005</Text>
+              </View>
+          </CollapsibleList>
+          <CollapsibleList
+              numberOfVisibleItems={0}
+              wrapperStyle={styles.wrapperCollapsibleList}
+              buttonContent={
+                <View style={styles.button}>
+                  <Text style={styles.buttonText}>Harold Frank Hall</Text>
+                </View>
+              }>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Harold Frank Hall 0000</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Harold Frank Hall 0001</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Harold Frank Hall 0002</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Harold Frank Hall 0003</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Harold Frank Hall 0004</Text>
+              </View>
+              <View style={styles.collapsibleItem}>
+                <Text style={styles.collapsibleItemText}>Harold Frank Hall 0005</Text>
+              </View>
+          </CollapsibleList>
         </View>
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-          <MonoText>Phelps 3501</MonoText>
-        </View>
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-          <MonoText>South Hall 1636</MonoText>
-        </View>
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-          <MonoText>North Hall 0000</MonoText>
-        </View>
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-          <MonoText>Harold Frank Hall 0000</MonoText>
-        </View>
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-          <MonoText>Broida Hall 0000</MonoText>
-        </View>
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-          <MonoText>Girvetz Hall 0000</MonoText>
-        </View>
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-          <MonoText>Life Sciences Building 0000</MonoText>
-        </View>
-      </View>
-
-    
-      <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-          <CollapsibleBathroomsList>CBL</CollapsibleBathroomsList>
-      </View>
     </ScrollView>
     </View>
   );
@@ -69,6 +141,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "rgba(0, 0, 0, 0.1)"
   },
+  button: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    backgroundColor: "rgba(170, 226, 240, 0.9)"
+  },
+  buttonText: {
+    fontSize: 35,
+    textAlign: 'center',
+  },
+  collapsibleItemText: {
+    fontSize: 24,
+    textAlign: 'center',
+  },
   contentContainer: {
     paddingTop: 30,
     justifyContent: "center",
@@ -82,4 +167,17 @@ const styles = StyleSheet.create({
   homeScreenFilename: {
     marginVertical: 7,
   },
+  wrapperCollapsibleList: {
+    flex: 1,
+    marginTop: 20,
+    overflow: "hidden",
+    backgroundColor: "#FFF",
+    borderRadius: 5
+  },
+  collapsibleItem: {
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: "#CCC",
+    backgroundColor: "rgba(170, 226, 240, 0.4)",
+    padding: 20
+  }
 });
