467639fd5ce52ff517a34c47bc250a5f75de61d2
aetherrine <aetherrine@users.noreply.github.com>
2020-02-19 00:18:37 -0800

    add config files

--- app/UCSBB/firebase.js
+++ app/UCSBB/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDbw-dMeNswFXPIQx9hrK21iNRAc7yU7dw",
-  authDomain: "test-6be40.firebaseapp.com",
-  databaseURL: "https://test-6be40.firebaseio.com",
-  projectId: "test-6be40",
-  storageBucket: "test-6be40.appspot.com",
-  messagingSenderId: "111873482173",
-  appId: "1:111873482173:web:57cd0b6bea08bbe70c06d4",
-  measurementId: "G-25N5EEQZXN"
+  apiKey: "AIzaSyCsvuNbyqaEIOSwxezzhlO5c88Ptkr0RJk",
+  authDomain: "bathroom-data.firebaseapp.com",
+  databaseURL: "https://bathroom-data.firebaseio.com",
+  projectId: "bathroom-data",
+  storageBucket: "bathroom-data.appspot.com",
+  messagingSenderId: "750327704519",
+  appId: "1:750327704519:web:262eb5a61728d74326c7e2",
+  measurementId: "G-0Y4LKS97K6"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
