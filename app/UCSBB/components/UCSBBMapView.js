53d6dd3cb2d928587c7c8053623bb685fe1ee136
DKW2 <32755558+DKW2@users.noreply.github.com>
2020-03-10 03:45:40 -0700

    dw - Changed map markers so that overlapping bathrooms can all be accessed/viewed

--- app/UCSBB/components/UCSBBMapView.js
+++ app/UCSBB/components/UCSBBMapView.js
@@ -38,43 +38,67 @@ export default class UCSBBMapView extends Component {
 			var load = true;
 			for(var i=0; i<buildingList.length; i++){
 			    rootRef.child(buildingList[i]).on("value", function(snapshot){
-			      var data = snapshot.val();
-			      var roomList = Object.keys(data);
+			      	var data = snapshot.val();
+			      	var roomList = Object.keys(data);
+					var coords = [];
+					var temp = [];
+					var allGender = false;
+			    	for (var j=0; j<roomList.length; j++){
+						var room = roomList[j];
+						var mtitle = buildingList[i] + " " + room; 
+						var lat = data[room].Latitude;
+						var long = data[room].Longitude;
+						if(!lat || !long){
+							load = false;
+						}
+						var gender = data[room].Gender;
+						var genderColor = "rgb(255,20,147)";
 
-			      for (var j=0; j<roomList.length; j++){
-					var room = roomList[j];
-					var mtitle = buildingList[i] + " " + room; 
-			        var lat = data[room].Latitude;
-					var long = data[room].Longitude;
-					if(!lat || !long){
-						load = false;
-					}
-					var gender = data[room].Gender;
-					var genderColor = "rgb(255,20,147)";
-
-					if(gender == "female"){
-						genderColor = "rgb(255,20,147)";
-						gender = "Female";
-					}
-					else if(gender == "male"){
-						genderColor = "#0000FF";
-						gender = "Male";
-					}
-					else{
-						genderColor = "#32CD32";
-						gender = "All Gender";
-					}
-					if(load)
-						views.push(
-							{ 
-								title: mtitle,
-								coordinates: {latitude: lat, longitude: long,}, 
-								pinColor: genderColor,  
-								description: gender,
+						if(gender == "female"){
+							genderColor = "rgb(255,20,147)";
+							gender = "Female";
+						}
+						else if(gender == "male"){
+							genderColor = "#0000FF";
+							gender = "Male";
+						}
+						else{
+							genderColor = "#32CD32";
+							gender = "All Gender";
+							allGender = true;
+						}
+						if(load){
+							var dupe = false;
+							for(var x = 0; x < coords.length; x++){
+								if(coords[x][0] == lat && coords[x][1] == long){
+									temp[x].title += ", " + room;
+									temp[x].description += ", " + room + " (" + gender + ")";
+									if(allGender)
+										temp[x].pinColor = "#32CD32";
+									else
+										temp[x].pinColor = "rgb(148,0,211)";
+									dupe = true;
+									break;
+								}
 							}
-						);
-					load = true;
-			      }
+							if(!dupe){
+								coords.push([lat,long]);
+								temp.push(
+								{ 
+									title: mtitle,
+									coordinates: {latitude: lat, longitude: long,}, 
+									pinColor: genderColor,  
+									description: room + " (" + gender + ")",
+								});
+								//console.log(temp);
+							}
+						}
+						load = true;
+					}
+					temp.forEach(function(mark){
+						views.push(mark);
+					}
+					);
 			    }
 			    );
 
@@ -107,23 +131,23 @@ export default class UCSBBMapView extends Component {
 		this.setState({region});
 	}
 
-	onRegionChangeComplete(region) {
-		if(region.latitude > LAT + 0.0052){
-			region.latitude = LAT + 0.0052;
-		}
-		if(region.latitude < LAT - 0.0072){
-			region.latitude = LAT - 0.0072;
-		}
-		if(region.longitude > LONG + 0.008){
-			region.longitude = LONG + 0.008;
-		}
-		if(region.longitude < LONG - 0.0094){
-			region.longitude = LONG - 0.0094;
-		}
-		this.map.animateCamera(
-			{center: {latitude: region.latitude, longitude: region.longitude}}
-		);
-	}
+	// onRegionChangeComplete(region) {
+	// 	if(region.latitude > LAT + 0.0052){
+	// 		region.latitude = LAT + 0.0052;
+	// 	}
+	// 	if(region.latitude < LAT - 0.0072){
+	// 		region.latitude = LAT - 0.0072;
+	// 	}
+	// 	if(region.longitude > LONG + 0.008){
+	// 		region.longitude = LONG + 0.008;
+	// 	}
+	// 	if(region.longitude < LONG - 0.0094){
+	// 		region.longitude = LONG - 0.0094;
+	// 	}
+	// 	this.map.animateCamera(
+	// 		{center: {latitude: region.latitude, longitude: region.longitude}}
+	// 	);
+	// }
 
 	render() {
 		return (
@@ -131,7 +155,7 @@ export default class UCSBBMapView extends Component {
 		  	  <MapView
 			    style = {styles.mapStyle}
 			    region = {this.state.region}
-			    onRegionChangeComplete={(region) => {this.onRegionChangeComplete(region)}}
+			    // onRegionChangeComplete={(region) => {this.onRegionChangeComplete(region)}}
 			    mapType = "standard"
 			    ref = {map => {this.map = map}}
 				provider = {MapView.PROVIDER_GOOGLE}
@@ -166,159 +190,3 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
     },
 });
-
-/*
-Previous building markers
-
-<Marker
-						coordinate={{latitude: 34.416161, longitude: -119.844639,}}
-						//title and description are temporary until onPress is set to open a pop up with bathrooms
-						title={"Phelps Hall"}
-						description={"Phelps 1501 Phelps 2501 Phelps 3501"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.412468, longitude: -119.849402,}}
-						title={"Arts Building"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.412799, longitude: -119.844914,}}
-						title={"Bioengineering Building"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.413207, longitude: -119.842200,}}
-						title={"Bren Hall"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.414081, longitude: -119.843084,}}
-						title={"Broida Hall"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.415422, longitude: -119.844569,}}
-						title={"Buchanan Hall"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.415429, longitude: -119.842616,}}
-						title={"Chemistry Building"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.415575, longitude: -119.845265,}}
-						title={"Ellison Hall"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.415263, longitude: -119.840039,}}
-						title={"Elings Hall"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.412146, longitude: -119.855813,}}
-						title={"Embarcadero Hall"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.415604, longitude: -119.841348,}}
-						title={"Engineering Science"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.414951, longitude: -119.841300,}}
-						title={"Engineering II"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.413495, longitude: -119.847191,}}
-						title={"Girvetz Hall"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.413832, longitude: -119.841375,}}
-						title={"Harold Frank Hall"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.413998, longitude: -119.850345,}}
-						title={"Humanities & Social Sciences Building"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.411358, longitude: -119.854811,}}
-						title={"Isla Vista Theater"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.414535, longitude: -119.846780,}}
-						title={"Kerr Hall"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.413710, longitude: -119.845535,}}
-						title={"Library"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.411864, longitude: -119.843791,}}
-						title={"Life Sciences Building"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.412295, longitude: -119.847303,}}
-						title={"Music Building"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.412596, longitude: -119.843781,}}
-						title={"Noble Hall"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.414995, longitude: -119.846526,}}
-						title={"North Hall"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.415455, longitude: -119.843582,}}
-						title={"Physical Science North"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.414542, longitude: -119.842637,}}
-						title={"Physical Science South"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.411928, longitude: -119.845078,}}
-						title={"Psychology Building"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.415213, longitude: -119.849500,}}
-						title={"Social Sciences & Media Studies"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.413877, longitude: -119.847609,}}
-						title={"South Hall"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.412406, longitude: -119.851322,}}
-						title={"Theater and Dance West"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.411589, longitude: -119.847969,}}
-						title={"University Center"}
-						description={"description"}
-		  	  />
-					<Marker
-						coordinate={{latitude: 34.413383, longitude: -119.843758,}}
-						title={"Webb Hall"}
-						description={"description"}
-		  	  />
-		  	  */
